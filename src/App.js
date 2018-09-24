3a3b2ea8b72d867587858d4b94c14539c4cebd45
Mightynumnums <deadcookies@gmail.com>
2018-09-24 16:10:41 -0400

    Get rid of type welcome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import './App.css';
-import { Route, HashRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from './components/Home.js'
 import Projects from './components/Projects.js'
 import Resume from './components/Resume.js'
 import Contacts from './components/Contacts.js'
-import LandingPage from './components/LandingPage'
+// import LandingPage from './components/LandingPage'
 import NotFound from './components/NotFound'
 
 
@@ -15,11 +15,12 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact={true} path='/' component={LandingPage} />
+          {/* <Route exact={true} path='/' component={LandingPage} /> */}
           <Route path='/home' component={Home} />
           <Route path='/projects' component={Projects} />
           <Route path='/resume' component={Resume} />
           <Route path='/contacts' component={Contacts} />
+          <Route exact={true} path='/' component={Home} />
           <Route component={NotFound} />
         </Switch>
       </Router >
