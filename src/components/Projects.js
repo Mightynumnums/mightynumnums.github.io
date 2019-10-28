2258a2f2fb64a7ecd602b9e40cc8340124e7fde2
Mightynumnums <deadcookies@gmail.com>
2019-10-28 08:37:10 -0400

    Updates the readme

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,40 +4,39 @@ import data from '../projectData.js'
 import './styles/projects.css'
 
 export default class Projects extends React.Component {
-constructor() {
-    super()
-    this.state = {
-        projects: data
+    constructor() {
+        super()
+        this.state = {
+            projects: data
+        }
     }
-}
 
-render() {
-    return (
-        <Page>
-            <div className='projectMain'>
-                { this.state.projects.map(project => {
-                return (
-                    <div className='container' key={ project.description }>
-                        <div className='hovereffect'>
-                            <a href={ project.links.webUrl } target='_blank'>
-                            <img className='projImg' src={ project.image.src }  alt={ project.image.alt } />
-                            <div className='overlay'>
-                                <p>{ project.description }</p>
+    render() {
+        return (
+            <Page>
+                <div className='projectMain'>
+                    { this.state.projects.map(project => {
+                    return (
+                        <div className='container' key={ project.description }>
+                            <div className='hovereffect'>
+                                <a href={ project.links.webUrl } target='_blank'>
+                                <img className='projImg' src={ project.image.src }  alt={ project.image.alt } />
+                                <div className='overlay'>
+                                    <p>{ project.description }</p>
+                                </div>
+                                </a>
                             </div>
-                            </a>
+                            <div className='container projLinks'>
+                                <a href={ project.links.ghUrl } target='_blank'>{ project.links.github }</a>
+                                <a href={ project.links.webUrl } target='_blank'>{ project.links.website }</a>
+                            </div>
                         </div>
-                        <div className='container projLinks'>
-                            <a href={ project.links.ghUrl } target='_blank'>{ project.links.github }</a>
-                            <a href={ project.links.webUrl } target='_blank'>{ project.links.website }</a>
-                        </div>
-                    </div>
-                )
-                })
-                }
-            </div>
-        </Page>
-    );
-}
+                    )})
+                    }
+                </div>
+            </Page>
+        );
+    }
 }
 
 
