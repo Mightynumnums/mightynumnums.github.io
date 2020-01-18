94c1d892157f8674057a2eceaf87bb0c25ce5d4b
Mightynumnums <deadcookies@gmail.com>
2020-01-18 13:03:33 -0500

    Adds open in a new window to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,8 @@ export default class Footer extends Component {
       <div className="footer">
         <div className='footer-logos'>
           <Link to='/contacts'><img src={mail} className="logo" alt="envelope mail logo" /></Link>
-          <a href='https://github.com/Mightynumnums'><img src={github} className="logo" alt="github logo" /></a>
-          <a href='//www.linkedin.com/in/aleksandra-shineleva/'><img src={linked} className="logo" alt="linkedIn logo" /></a>
+          <a href='https://github.com/Mightynumnums' target="_blank"><img src={github} className="logo" alt="github logo" /></a>
+          <a href='//www.linkedin.com/in/aleksandra-shineleva/' target="_blank"><img src={linked} className="logo" alt="linkedIn logo" /></a>
         </div>
         <p id='copy'>&copy;Aleks Shineleva | 2020</p>
       </div>
